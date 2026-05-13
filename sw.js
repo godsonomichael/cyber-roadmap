90744ae5be6fa43d1dca259c3757a57850acdc94
godsonomichael <godsonomichael@gmail.com>
2026-05-13 20:41:13 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'crp-v1';
+const ASSETS = [
+  '/cyber-roadmap/',
+  '/cyber-roadmap/index.html',
+  '/cyber-roadmap/manifest.json',
+  '/cyber-roadmap/logo.jpg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('/cyber-roadmap/index.html'));
+    })
+  );
+});
